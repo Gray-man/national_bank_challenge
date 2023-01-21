a569223b7f7edd54237eb3157d0aaae077871c78
Gray-man <52508343+Gray-man@users.noreply.github.com>
2023-01-21 17:25:18 -0500

    Add files via upload

---
+++ react-app/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import JsonDataDisplay from './MyPractice/table.jsx'
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello</h1>
+        <JsonDataDisplay/>
+    </div>
+  );
+}
+
+export default App;
